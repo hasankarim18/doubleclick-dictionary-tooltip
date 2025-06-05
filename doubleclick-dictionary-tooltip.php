f85a68c54ad18f9b642a5b9e07a21e8ecb5a4959
hasankarim18 <hallysust@gmail.com>
2025-06-05 18:36:58 +0600

    first commit

---
+++ doubleclick-dictionary-tooltip.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Plugin Name: DoubleClick Dictionary Tooltip
+Plugin URI: https://example.com/doubleclick-dictionary-tooltip
+Description: Adds a tooltip with dictionary definitions when users double-click words.
+Version: 1.0.0
+Author: Your Name
+Author URI: https://example.com
+License: GPL2
+Text Domain: doubleclick-dictionary-tooltip
+*/
+
